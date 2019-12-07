7b41e83f4c9899c8a1258b16a836322b8a492ef5
Svante v. Erichsen <svante.v.erichsen@web.de>
2019-12-07 08:18:29 +0100

    day 7, b

--- 7.lisp
+++ 7.lisp
@@ -13,18 +13,22 @@
 
 (defun aoc7a (&optional (amp (coerce (read-integers "7")
                                      'vector)))
+  (max-output amp #'run-amps #(0 1 2 3 4)))
+
+(defun max-output (amp runner possible-phases)
   (let ((outputs ()))
     (map-permutations (lambda (phases)
-                        (push (run-amps (copy-seq amp)
-                                        phases)
+                        (push (funcall runner
+                                       (copy-seq amp)
+                                       phases)
                               outputs))
-                      #(0 1 2 3 4))
+                      possible-phases)
     (reduce #'max outputs)))
 
 (defun run-amps (program phases)
   (loop :for phase :across phases
         :for input := 0 :then output
-        :for output := (amplifier program phase input)
+        :for output := (amplifier (copy-seq program) phase input)
         :finally (return output)))
 
 (defun amplifier (program phase input)
@@ -34,3 +38,22 @@
     (chanl:send in input)
     (chanl:recv out)))
 
+(defun aoc7b (&optional (amp (coerce (read-integers "7")
+                                     'vector)))
+  (max-output amp #'run-amps-feedback #(5 6 7 8 9)))
+
+(defun run-amps-feedback (program phases)
+  (let ((amps (loop :repeat 5
+                    :collect (intcode (copy-seq program)
+                                      :interactivep nil))))
+    (loop :for (in out) :in amps
+          :for phase :across phases
+          :do (chanl:send in phase))
+    (chanl:send (first (first amps)) 0)
+    (loop :for sender :in (apply #'circular-list amps)
+          :for receiver :in (rest (apply #'circular-list amps))
+          :for signal := (chanl:recv (second sender))
+          :for output := (if (numberp signal) signal output)
+          :until (eq signal :end)
+          :do (chanl:send (first receiver) signal)
+          :finally (return output))))
