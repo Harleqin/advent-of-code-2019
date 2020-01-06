d5d66fe3ba61314bde9d1369be2ecfdb74a5d96f
Svante v. Erichsen <svante.v.erichsen@web.de>
2020-01-06 01:00:13 +0100

    day 17, b

--- 17.lisp
+++ 17.lisp
@@ -23,12 +23,6 @@
     (values map
             (alignment map))))
 
-(defun read-map (view)
-  (read-matrix (with-input-from-string (v view)
-                 (loop :for line := (read-line v nil)
-                       :while (some-> line length plusp)
-                       :collect line))))
-
 (defun alignment (map)
   (loop :for y :upfrom 1 :below (1- (array-dimension map 0))
         :sum (loop :for x :from 1 :below (1- (array-dimension map 1))
@@ -41,35 +35,29 @@
                      :sum (* y x))))
 
 (defun aoc17b (&optional (program (coerce (read-integers "17")
-                                          'vector))
-               &aux (map (aoc17a program)))
+                                          'vector)))
   (setf (aref program 0) 2)
-  (let ((path (trace-path map)))
-    path))
+  (let ((instructions "A,A,B,C,A,C,A,B,C,B
+R,12,L,8,R,6
+R,12,L,6,R,6,R,8,R,6
+L,8,R,8,R,6,R,12
+n
+")
+        (ip 0))
+    (flet ((input ()
+             (prog1 (char-code (char instructions ip))
+               (incf ip)))
+           (output (n)
+             (cond ((eq :end n) nil)
+                   ((< n 128)
+                    (princ (code-char n)))
+                   (t
+                    (format t "~%-> ~a~%" n)))))
+      (aoc-2019/intcode:intcode-core program
+                                     #'input
+                                     #'output))))
 
-(defun trace-path (map)
-  (loop :for pos := (2d-pos-if (lambda (c)
-                                 (member c '(#\^ \> #\< #\v)))
-                               map)
-          :then next-pos
-        :for dir := (ecase (map-ref map pos)
-                      (#\^ #(0 -1))
-                      (#\> #(1 0))
-                      (#\v #(0 1))
-                      (#\< #(-1 0)))
-          :then next-dir
-        :for (next-pos next-dir moves) := (find-path map pos dir)
-        :while next-pos
-        :append moves))
 
-(defun find-path (map pos dir)
-  (loop :for (moves . d) :in (list (cons '(1) dir)
-                                  (cons '(#\R 1) (turn-right/screen dir))
-                                  (cons '(#\L 1) (turn-left/screen dir)))
-        :for p := (map 'vector #'+ pos d)
-        :when (and (array-in-bounds-p map (aref p 1) (aref p 0))
-                   (char= #\# (map-ref map p)))
-          :do (return (list p d moves))))
 
 (defun turn-left/screen (dir)
   (v*m dir #2a((0 -1)
